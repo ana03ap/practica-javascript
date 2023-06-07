48444d18025883ab7258325f1028d6ac1e890899
ana03ap <anxrdilx03@gmail.com>
2023-06-06 20:55:17 -0500

    Ejercicios switch
    empezando ejercicios de switch case

---
+++ ejerciciosSwitch.js
@@ -0,0 +1,86 @@
+//Ejercicios de switch case
+
+// Este ejemplo utiliza un switch case para determinar el día de la semana en función de un número dado.
+// Dependiendo del valor del número, se muestra un mensaje correspondiente al día de la semana.
+
+function ejercicio1(num){
+    switch (num){
+        case 1:
+            console.log("Lunes")
+            break;
+        case 2:
+            console.log("Martes")
+            break;
+        case 3:
+            console.log("Miércoles")
+            break;
+        case 4:
+            console.log("Jueves")
+            break;
+        case 5:
+            console.log("Viernes")
+            break;
+        case 6:
+            console.log("Sabado")
+            break;
+        case 7:
+            console.log("Domingo")
+            break;
+        default:
+            console.log("Número no disponible!!!!")
+            break;
+    }
+}
+
+// ejercicio1(1);
+// console.log("ana")
+
+//Ejercicio 2
+// En este caso, el switch case se utiliza para identificar el mes del año en función de un número dado.
+//  Se muestra un mensaje según el número proporcionado correspondiente al mes.
+function ejercicio2(num){
+    switch (num){
+        case 1:
+            console.log("Enero")
+            break;
+        case 2:
+            console.log("Febrero")
+            break;
+        case 3:
+            console.log("Marzo")
+            break;
+        case 4:
+            console.log("Abril")
+            break;
+        case 5:
+            console.log("Mayo")
+            break;
+        case 6:
+            console.log("Junio")
+            break;
+        case 7:
+            console.log("Julio")
+            break;
+        case 8:
+            console.log("Agosto")
+            break;
+        case 9:
+            console.log("Septiembre")
+            break;
+        case 10:
+            console.log("Octubre")
+            break;
+        case 11:
+            console.log("Noviembre")
+            break;
+        case 12:
+            console.log("Diciembre")
+            break;
+        default:
+            console.log("Número no disponible!!!!")
+            break;
+    }
+}
+
+//Tercer ejercicio 
+
